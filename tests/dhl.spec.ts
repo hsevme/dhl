f999cbaa3249833766d567231e45e3ed372b6c69
Sev <49195313+hsevme@users.noreply.github.com>
2024-06-11 15:28:35 +0200

    Update dhl.spec.ts

--- tests/dhl.spec.ts
+++ tests/dhl.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, Locator } from '@playwright/test';
 
-test('mock api calls', async ({ page }) => {
+test('test dhl status', async ({ page }) => {
   await page.goto('https://www.dhl.de/de/privatkunden/pakete-empfangen/verfolgen.html?lang=de&idc=00340434666024752624&rfn=&extendedSearch=true');
   const infoText = page.locator('.infoText');
   await expect(infoText).toHaveText('Status offen');
