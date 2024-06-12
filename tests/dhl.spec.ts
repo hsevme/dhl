e7fe62bfeb10eee22c98a5274c56a0bb50ce02dc
hsevme <49195313+hsevme@users.noreply.github.com>
2024-06-12 10:09:56 +0200

    Edit expected infoText

--- tests/dhl.spec.ts
+++ tests/dhl.spec.ts
@@ -1,7 +1,14 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, expect, defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    // Capture screenshot after each test failure.
+    screenshot: 'only-on-failure',
+  }
+});
 
 test('test dhl status', async ({ page }) => {
   await page.goto('https://www.dhl.de/de/privatkunden/pakete-empfangen/verfolgen.html?lang=de&idc=00340434666024752624&rfn=&extendedSearch=true');
   const infoText = page.locator('.infoText');
-  await expect(infoText).toHaveText('Status offen');
+  await expect(infoText).toHaveText('Vorbereitung für Weitertransport');
 });
